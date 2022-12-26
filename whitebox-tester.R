3a738b6c61f16c6b31e2af467d3784e6b858ad3b
Cole <colery@oregonstate.edu>
2022-12-26 15:54:59 -0800

    Started branch to test whitebox tools watershed delineation

---
+++ whitebox-tester.R
@@ -0,0 +1,8 @@
+# Testing script for watershed delineation using whitebox tools and NHDPlusHR
+# flow accumulation raster. The goal is to give an input of a few points and have
+# an output file of delineated watershed polygons in GPKG format? Format is still
+# up for grabs I think. Not sure what would be the best default for this.
+
+# This will require figuring out how to install whitebox tools as a package depenency
+# for another package - I'm not sure how hard this actually is, but I don't know
+# how to do it.
